6871e1106e2c7299ead70d0a9119e26af33039e1
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2021-01-22 13:38:35 +0100

    [gasPrice] use waffle for testss

--- src/gas-price-updater.spec.ts
+++ src/gas-price-updater.spec.ts
@@ -4,11 +4,9 @@ import sinon from "sinon";
 import { updateGasPrice } from "./gas-price-updater";
 import axios from "axios";
 import { getGasPrice, setGasPrice } from "./gas-price";
-import { BigNumber, ethers } from "ethers";
-import Ganache, { JsonRpcPayload, JsonRpcResponse } from "ganache-core";
-import { JsonRpcFetchFunc } from "@ethersproject/providers/src.ts/web3-provider";
 import { GAS_MULTIPLIER } from "./config";
-import { Provider } from "@ethersproject/providers";
+import { MockProvider } from "@ethereum-waffle/provider";
+import { BigNumber } from "ethers";
 
 const sandbox = sinon.createSandbox();
 
@@ -22,7 +20,7 @@ describe("gas price updater test suite", () => {
         sandbox.stub(axios, "get").returns(
             Promise.resolve({
                 data: { average: 450 },
-            }),
+            })
         );
         await updateGasPrice();
         expect(getGasPrice()!.toString()).to.be.eq("45000000000");
@@ -30,7 +28,7 @@ describe("gas price updater test suite", () => {
         sandbox.stub(axios, "get").returns(
             Promise.resolve({
                 data: { average: 750 },
-            }),
+            })
         );
         await updateGasPrice();
         expect(getGasPrice()!.toString()).to.be.eq("75000000000");
@@ -40,7 +38,7 @@ describe("gas price updater test suite", () => {
         sandbox.stub(axios, "get").returns(
             Promise.resolve({
                 data: { average: 450 },
-            }),
+            })
         );
         await updateGasPrice();
         expect(getGasPrice()!.toString()).to.be.eq("45000000000");
@@ -55,7 +53,7 @@ describe("gas price updater test suite", () => {
         sandbox.stub(axios, "get").returns(
             Promise.resolve({
                 data: { average: 450 },
-            }),
+            })
         );
         await updateGasPrice();
         expect(getGasPrice()!.toString()).to.be.eq("45000000000");
@@ -70,34 +68,15 @@ describe("gas price updater test suite", () => {
         sandbox.stub(log, "error").returns();
         sandbox.stub(axios, "get").returns(Promise.resolve({ data: {} }));
         const networkGasPrice = BigNumber.from("20000000000");
-        const ganacheProvider = Ganache.provider({
-            gasPrice: networkGasPrice.toString()
+        const provider = new MockProvider({
+            ganacheOptions: {
+                gasPrice: networkGasPrice.toString(),
+            },
         });
-        const provider = createEthersProviderFromGanache(ganacheProvider);
         await updateGasPrice(provider);
-        const expectedGasPrice = BigNumber.from(networkGasPrice).mul(GAS_MULTIPLIER).div(100);
+        const expectedGasPrice = BigNumber.from(networkGasPrice)
+            .mul(GAS_MULTIPLIER)
+            .div(100);
         expect(getGasPrice()!.toString()).to.be.eq(expectedGasPrice.toString());
     });
 });
-
-const createEthersProviderFromGanache = (ganacheProvider: Ganache.Provider): Provider => {
-    const rpcFunc: JsonRpcFetchFunc = (method: string, params?: Array<any>): Promise<any> => {
-        const payload: JsonRpcPayload = {
-            jsonrpc: "",
-            method,
-            params: params && params || [],
-        };
-        return new Promise((s, e) => {
-            const callback = (error: Error | null, result?: JsonRpcResponse) => {
-                if (error) return e(error);
-                if(result) return s(result.result);
-                return s(null);
-            };
-            ganacheProvider.send(
-                payload,
-                callback,
-            );
-        });
-    };
-    return new ethers.providers.Web3Provider(rpcFunc);
-}
